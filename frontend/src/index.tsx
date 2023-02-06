10309b17e6099e2a9eafd5edbbe0c23ea1bf3552
Reza Javan <javan.reza1380@gmail.com>
2023-02-06 18:32:39 +0330

    Create routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import "./styles/index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "pages/Error";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+//Create routes
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <Error />,
+	},
+]);
+
+const root = ReactDOM.createRoot(
+	document.getElementById("root") as HTMLElement
 );
-
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
