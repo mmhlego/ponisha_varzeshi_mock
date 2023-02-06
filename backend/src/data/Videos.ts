04193f691562a711384d1fa89f5074d73deac476
mmhlego <mmhlego@gmail.com>
2023-02-06 23:57:19 +0330

    Completed /videos and /coaches and /seed/coaches api Added models Added data files

---
+++ backend/src/data/Videos.ts
@@ -0,0 +1,67 @@
+import Video from "../model/Video";
+
+export const Videos: Video[] = [
+	{
+		id: 1,
+		title: "بررسی سرویس های اپل فیتنس پلاس Apple Fitness Plus",
+		description:
+			"میله تمرین کش دار Gym Stick ( جیم استیک ) اولین بار به عنوان رشته‌ای ورزشی در کشور فنلاند در سال ۱۹۹۷ از یک موسسه فنلاندی ورزشی معرفی شد",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/49647471_15s.mp4",
+	},
+	{
+		id: 2,
+		title: "بررسی جزئیات و ویژگی های تردمیل پلوتون peloton tread",
+		description: "بررسی جزئیات و ویژگی های تردمیل پلوتون peloton tread",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/49604662_15s.mp4",
+	},
+	{
+		id: 3,
+		title: "تبلیغ دستگاه تمرین پارویی پلوتون peloton row",
+		description: "تبلیغ دستگاه تمرین پارویی پلوتون peloton row",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/49604629_15s.mp4",
+	},
+	{
+		id: 4,
+		title: "آموزش انواع تمرین های ایستاده با جیم استیک (gym stick)",
+		description: "آموزش انواع تمرین های ایستاده با جیم استیک (gym stick)",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/48405994_15s.mp4",
+	},
+	{
+		id: 5,
+		title: "۴۴ تا از بهترین تمرینات قدرتی با توپ مدیسن بال",
+		description: "۴۴ تا از بهترین تمرینات قدرتی با توپ مدیسن بال",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/47298372_15s.mp4",
+	},
+	{
+		id: 6,
+		title: "40 تا از بهترین تمرین ها با مدیسن بال (medicine ball)",
+		description: "40 تا از بهترین تمرین ها با مدیسن بال (medicine ball)",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/47274869_15s.mp4",
+	},
+	{
+		id: 7,
+		title: "ورزش روتین صبحگاهی برای عضلات مرکزی بدن بدون نیاز به تجهیزات",
+		description:
+			"ورزش روتین صبحگاهی برای عضلات مرکزی بدن بدون نیاز به تجهیزات",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/43258533_15s.mp4",
+	},
+	{
+		id: 8,
+		title: "۱۰ دقیقه ورزش صبحگاهی بدون نیاز به تجهیزات و وسایل",
+		description: "۱۰ دقیقه ورزش صبحگاهی بدون نیاز به تجهیزات و وسایل",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/39696654_15s.mp4",
+	},
+	{
+		id: 9,
+		title: "تمرین های شکم برای ورزش در خانه",
+		description: "تمرین های شکم برای ورزش در خانه",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/39150166_15s.mp4",
+	},
+	{
+		id: 10,
+		title: "تمرینهای هادی چوپان و مربی هنی رامبد برای مسابقات مستر المپیا",
+		description:
+			"تمرینهای هادی چوپان و مربی هنی رامبد برای مسابقات مستر المپیا",
+		videoUrl: "https://static.cdn.asset.aparat.com/avt/38439999_15s.mp4",
+	},
+];
---
+++ backend/src/model/Video.ts
@@ -0,0 +1,6 @@
+export default interface Video {
+	id: number;
+	title: string;
+	description: string;
+	videoUrl: string;
+}
